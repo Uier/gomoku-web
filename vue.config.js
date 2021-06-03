34ef54d1b2aa9035f5d13976360d98875755495e
Uier <ap9940506@gmail.com>
2021-06-03 14:37:38 +0800

    recover public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const WorkerPlugin = require('worker-plugin')
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production' ? '/gomoku-web' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/gomoku-web' : '/',
   configureWebpack: {
     output: {
       globalObject: "this"
